89d2b6b29860f2e27f4fc9b74d052a8af46ff77c
Pummy B <66464883+pummy12345@users.noreply.github.com>
2023-02-12 13:07:42 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import "./styles/main.scss";
-import { BrowserRouter } from "react-router-dom";
+import { useState } from "react";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+const Navbar = ({ onClickSearch }) => {
+  const [searchQuery, setSearchQuery] = useState("");
+  return (
+    <div className="navbar">
+      <div className="navbar__logo">MovieMania</div>
+      <div className="navbar__search">
+        <input
+          placeholder="Search"
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+
+        <button type="submit" onClick={() => onClickSearch(searchQuery)}>
+          Search
+        </button>
+      </div>
+    </div>
+  );
+};
+// <i className="navbar__search__icon fa-sharp fa-solid fa-magnifying-glass"></i>
+
+export default Navbar;
